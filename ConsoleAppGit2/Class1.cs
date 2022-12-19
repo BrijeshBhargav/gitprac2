27c6a7ff61616ffa4ad8d38187c48dac47efb227
BrijeshBhargav <gollabrijesh2001@gmail.com>
2022-12-19 12:13:24 +0530

    commit1

---
+++ ConsoleAppGit2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGit2
+{
+    internal class Class1
+    {
+        public void test()
+        {
+            Console.WriteLine("hello");
+        }
+        public void test2()
+        {
+            Console.WriteLine("hello parctice");
+        }
+    }
+}
